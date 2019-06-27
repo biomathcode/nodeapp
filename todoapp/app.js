d818cc5ef5bdf487659640462359798930a177e1
pratiksharm <sharma.pratik2016@gmail.com>
2019-06-27 13:36:23 +0530

    added express, ejs and other stuff

---
+++ todoapp/app.js
@@ -0,0 +1,11 @@
+const express = require(express);
+const app  = express();
+const todoController = require('./controllers/todocontroller');
+//set up template engine
+app.set('view engine', 'ejs');
+//static files
+app.use(express.static('./public'));
+//fire controllers
+todoController(app);
+app.listen(3000);
+console.log('You are listening at port 3000')
---
+++ todoapp/controllers/todocontroller.js
@@ -0,0 +1,16 @@
+module.exports = function(app) {
+//all the request handlers
+//for get the info
+app.get('/todo', function(req, res) {
+
+});
+//for posting the info
+app.post('/todo', function(req, res) {
+    
+});
+//for deleting staff
+app.delete('/todo', function(req, res) {
+
+});
+
+};
