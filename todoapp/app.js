659c003dc85b05d0700f469fa67fee292a67d071
pratiksharm <sharma.pratik2016@gmail.com>
2019-07-05 11:05:01 +0530

    -added todoController

--- todoapp/app.js
+++ todoapp/app.js
@@ -1,4 +1,4 @@
-const express = require(express);
+const express = require('express');
 const app  = express();
 const todoController = require('./controllers/todocontroller');
 //set up template engine
--- todoapp/controllers/todocontroller.js
+++ todoapp/controllers/todocontroller.js
@@ -1,15 +1,27 @@
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+const data = [{item: 'get milk'},{item: 'walk dog'}, {item:'kick some coding ass.'}]
 module.exports = function(app) {
 //all the request handlers
 //for get the info
 app.get('/todo', function(req, res) {
+    res.render('todo', {todos: data});
 
 });
 //for posting the info
-app.post('/todo', function(req, res) {
+app.post('/todo',urlencodedParser, function(req, res) {
+    data.push(req.body);
+    res.json(data);
     
 });
 //for deleting staff
-app.delete('/todo', function(req, res) {
+app.delete('/todo/:item', function(req, res) {
+    data = data.filter(function(todo){
+        return todo.item.replace(/ /g, '-') !== req.params.item;
+    });
+    res.json(data);
+
 
 });
 
